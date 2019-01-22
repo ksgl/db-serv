0b92a815516108d9f27c44128e91294f9c87d147
ksgl <ksenia.glazacheva@gmail.com>
2019-01-23 02:45:59 +0300

    sync on

--- database/database.go
+++ database/database.go
@@ -1,25 +1,30 @@
 package database
 
 import (
+	"sync"
+
 	"github.com/jackc/pgx"
 )
 
 var DB *pgx.ConnPool
+var once sync.Once
 
 const maxConn = 50
 
 func Connect() (conn *pgx.ConnPool) {
-	connConfig := pgx.ConnConfig{
-		User:     "ksu",
-		Password: "pswd",
-		Host:     "localhost",
-		Port:     5432,
-		Database: "parkdb",
-	}
+	once.Do(func() {
+		connConfig := pgx.ConnConfig{
+			User:     "ksu",
+			Password: "pswd",
+			Host:     "localhost",
+			Port:     5432,
+			Database: "parkdb",
+		}
 
-	DB, _ = pgx.NewConnPool(pgx.ConnPoolConfig{
-		ConnConfig:     connConfig,
-		MaxConnections: maxConn,
+		DB, _ = pgx.NewConnPool(pgx.ConnPoolConfig{
+			ConnConfig:     connConfig,
+			MaxConnections: maxConn,
+		})
 	})
 
 	return DB
